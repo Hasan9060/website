2a6a5b692d3d287d02e5a0e4c37350bc0b695fa2
hasan9060 <hasanrafay2006@gmail.com>
2025-01-23 22:03:13 +0500

    update5

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -4,6 +4,6 @@ import { createClient } from 'next-sanity'
 export const client = createClient({
   projectId: '2srh4ekv',
   dataset: 'production',
-  apiVersion: '2023-01-01',
+  apiVersion: 'skz6lWFJkAgpfrjXgwK8Tb6UBsTpRcSwzsQawON5Qps118XQdODrtVLdyXySTgJqC7rhPUKAOzb9prGs2aORcV0IICFN6pLKCLW2G0P7u5rExc8E92fzYp0UMuro6VpCzm51svtpWMCniHWaEiZAeJApDrYyIXgO5Uar4GLM2QPxFsswwZnU',
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
